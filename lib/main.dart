0f9f15c3c16a8dd5190775be8a2591a32a48be58
Md Abdul Hai <mdabdulhai2506@gmail.com>
2024-09-03 20:16:58 +0600

    body working start

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+void main() {
+  runApp(const CrudApp());
+}
+
+class CrudApp extends StatelessWidget {
+  const CrudApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.indigo,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
