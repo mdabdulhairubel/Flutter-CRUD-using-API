dd4e601b527ad11698298f89c8ac10a187051584
Md Abdul Hai <mdabdulhai2506@gmail.com>
2024-09-03 20:47:50 +0600

    list view create done

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,11 +12,32 @@ class HomeScreen extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
-      body: const Center(
-        child: Text(
-          "Hello World",
-          style: TextStyle(fontSize: 24),
-        ),
+      body: ListView.separated(
+        separatorBuilder: (context, index) {
+          return const Divider(
+            thickness: 2,
+            color: Colors.grey,
+          );
+        },
+        itemCount: 20,
+        itemBuilder: (context, index) {
+          return const ListTile(
+            leading: CircleAvatar(),
+            title: Text(
+              "Product Name",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Wrap(
+              spacing: 30,
+              children: [
+                Text("Product Code", style: TextStyle(color: Colors.green),),
+                Text("Unit Price", style: TextStyle(color: Colors.red),),
+                Text("Quantity", style: TextStyle(color: Colors.indigo),),
+                Text("Total Price", style: TextStyle(color: Colors.amber),),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
